580b14c098b0ae444527ff86a67c1f20e08869b1
Nhut Huynh <nhuthm080280@gmail.com>
2017-05-20 15:36:41 +0700

    add lab05

---
+++ lab05/index.php
@@ -0,0 +1,12 @@
+<?php include_once("header.php");?>
+<ul>
+	<li>
+		<a href="/lab05/list_product.php">Danh sach san pham</a>
+	</li>
+	
+	<li>
+		<a href="/lab05/add_product.php">Them san pham</a>
+	</li>
+</ul>
+<?php include_once("footer.php");?>
+

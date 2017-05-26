b9db01b6007a6443534b1bf4195b6e5c80c337c7
Huynh <nhuynh8@vdc.csc.com>
2017-05-26 15:46:16 +0700

    update register.php,add index.js

--- foodstore/register.php
+++ foodstore/register.php
@@ -29,64 +29,94 @@ if(isset($_POST['btn-signup']))
     }
 }
 ?>
+        <style>
+          .big-btn-b {
+            backface-visibility: hidden;
+            background: #1286bc;
+            border: 0;
+            border-radius: 2px;
+            box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+            color: #fff;
+            display: inline-block;
+            font-size: 16px;
+            font-weight: 500;
+            moz-border-radius: 2px;
+            moz-osx-font-smoothing: grayscale;
+            padding: 10px 30px;
+            position: relative;
+            text-align: center;
+            text-decoration: none;
+            text-transform: uppercase;
+            transform: translateZ(0);
+            transition-duration: .5s;
+            transition-property: color;
+            vertical-align: middle;
+            webkit-backface-visibility: hidden;
+            webkit-border-radius: 2px;
+            webkit-transform: translateZ(0);
+            webkit-transition-duration: .5s;
+            webkit-transition-property: color;
+          }
+        </style>
         <!-- gallery -->
         <div class="gallery">
           <div class="container">
             <div class="row">
               <div class="col-md-6 col-md-offset-3">
-              <div class="panel panel-default">
-                <div class="panel-body">
-                  <div class="row">
-                    <div class="col-lg-12">
-                      <h2>Register</h2>
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    <div class="row">
+                      <div class="col-lg-12">
+                        <h1 class="atc-headline">Đăng ký thành viên</h1>
 
-                      <br/>
+                        <br/>
 
-                      <form class="form-horizontal" method="post" enctype="multipart/form-data">
-                        <div class="form-group">
+                        <form class="form-horizontal" method="post" enctype="multipart/form-data">
+                          <div class="form-group">
 
-                          <div class="col-lg-12">
-                            <input type="text" class="form-control" name="txtname" placeholder="User name">
+                            <div class="col-lg-12">
+                              <input type="text" class="form-control" name="txtname" placeholder="User name" required>
+                            </div>
                           </div>
-                        </div>
 
-                        <div class="form-group">
+                          <div class="form-group">
 
-                          <div class="col-lg-12">
-                            <input type="email" class="form-control" name="txtemail" placeholder="Email">
+                            <div class="col-lg-12">
+                              <input type="email" class="form-control" name="txtemail" placeholder="Email" required>
+                            </div>
                           </div>
-                        </div>
 
-                        <div class="form-group">
-                          <div class="col-lg-12">
-                            <input type="password" class="form-control" name="txtpass" placeholder="Password">
+                          <div class="form-group">
+                            <div class="col-lg-12">
+                              <input type="password" class="form-control" name="txtpass" placeholder="Password" required id="password">
+                            </div>
                           </div>
-                        </div>
 
-                        <div class="form-group">
-                          <div class="col-lg-12">
-                            <input type="password" class="form-control" name="txtconfirmpass" placeholder="Confirm Password">
+                          <div class="form-group">
+                            <div class="col-lg-12">
+                              <input type="password" class="form-control" name="txtconfirmpass" placeholder="Confirm Password" required id="confirm_password">
+                            </div>
                           </div>
-                        </div>
 
-                        <div class="form-group">
-                          <div class="col-lg-12">
-                            <input type="submit" name="btn-signup" value="Sign Up" class="btn btn-success" />
+                          <div class="form-group">
+                            <div class="col-lg-12">
+                              <input type="submit" name="btn-signup" value="Sign Up" class="big-btn-b" />
+                            </div>
                           </div>
-                        </div>
 
-                      </form>
+                        </form>
 
+                      </div>
                     </div>
+
+
                   </div>
-               
-               
                 </div>
-              </div>
               </div>
             </div>
           </div>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="js/index.js"></script>
 
-
-        </div>
+        </div>    
         <?php include_once("footer.php"); ?>
